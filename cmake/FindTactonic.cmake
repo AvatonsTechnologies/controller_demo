8c0cff0dda6673c530b5d6cc01ef770019f6bf8d
Matthew McAllister <matthew.mcallister@berkeley.edu>
2016-01-19 13:09:55 -0800

    Add build script

---
+++ cmake/FindTactonic.cmake
@@ -0,0 +1,29 @@
+# This module looks for Tactonic's pressure sensor libraries.
+# If cmake isn't finding these for you, check that the Tactonic_DIR
+# environment variable is defined and it contains include/Tactonic.h
+# and lib/Tactonic.lib.
+
+find_path(Tactonic_INCLUDE_DIR
+    Tactonic.h
+    HINTS "$ENV{Tactonic_DIR}/include"
+    )
+
+find_library(Tactonic_LIBRARY
+    tactonic
+    HINTS "$ENV{Tactonic_DIR}"
+    )
+
+find_library(TactonicTouch_LIBRARY
+    tactonicTouch
+    HINTS "$ENV{Tactonic_DIR}"
+    )
+
+set(Tactonic_LIBRARIES "${Tactonic_LIBRARY}" "${TactonicTouch_LIBRARY}")
+set(Tactonic_INCLUDE_DIRS "${Tactonic_INCLUDE_DIR}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Tactonic DEFAULT_MSG Tactonic_LIBRARY
+    TactonicTouch_LIBRARY Tactonic_INCLUDE_DIR)
+
+mark_as_advanced(Tactonic_INCLUDE_DIR Tactonic_LIBRARY)
+
